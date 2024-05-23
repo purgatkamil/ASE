4e64bfd4fa2e8ced853cdd4c9d6f246769ceb7ba
Piotr Rosiak <rosiakpiotr3@gmail.com>
2024-05-23 12:56:22 +0200

    Working pulse tracker

--- ase-encoder-arduino/src/main.cpp
+++ ase-encoder-arduino/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 // Definicja pinów dla sterowania silnikami
-#define IN1 25  // Pin IN1 dla kanału A
-#define IN2 26  // Pin IN2 dla kanału A
-#define IN3 27  // Pin IN3 dla kanału B
-#define IN4 14  // Pin IN4 dla kanału B
-#define ENA 12  // PWM dla kanału A
-#define ENB 13  // PWM dla kanału B
+#define IN1 25          // Pin IN1 dla kanału A
+#define IN2 26          // Pin IN2 dla kanału A
+#define IN3 27          // Pin IN3 dla kanału B
+#define IN4 14          // Pin IN4 dla kanału B
+#define ENA 12          // PWM dla kanału A
+#define ENB 13          // PWM dla kanału B
 #define ENCODER_PIN1 16 // Pin enkodera 1 - GPIO 0 na ESP32
 #define ENCODER_PIN2 17 // Pin enkodera 2 - GPIO 2 na ESP32
 
@@ -46,30 +46,47 @@ float alpha = 0.1;
 class PIController
 {
 public:
-  PIController(double _k, double _i, double _i_low, double _i_high): 
-    k(_k), i(_i), imin(_i_low), imax(_i_high) {
+  PIController(double _k, double _i, double _i_low, double _i_high) : k(_k), i(_i), imin(_i_low), imax(_i_high) {}
 
-    } 
-
-  double update(double error, double dt) {
+  double update(double error, double dt)
+  {
     integral = constrain(integral + error * dt, imin, imax);
-    return error*k + integral*i;
+    return error * k + integral * i;
   }
+
 private:
-  double k,i;
+  double k, i;
   double integral;
-  double imin,imax; // Integral min and max
+  double imin, imax; // Integral min and max
 };
 
-void IRAM_ATTR updateEncoder1() {
-  currentPosition1++;
+int8_t dirA=1, dirB=1;
+
+void IRAM_ATTR updateEncoder1()
+{
+  currentPosition1+=dirA;
 }
 
-void IRAM_ATTR updateEncoder2() {
-  currentPosition2++;
+void IRAM_ATTR updateEncoder2()
+{
+  currentPosition2+=dirB;
 }
 
-void setup() {
+#define PI_K_A 1.5
+#define PI_I_A 0.8
+#define PI_IMIN_A -200.0 / PI_I_A
+#define PI_IMAX_A 200.0 / PI_I_A
+
+#define PI_K_B 1.5
+#define PI_I_B 0.8
+#define PI_IMIN_B -200.0 / PI_I_B
+#define PI_IMAX_B 200.0 / PI_I_B
+
+PIController piA(PI_K_A, PI_I_A, PI_IMIN_A, PI_IMAX_A);
+PIController piB(PI_K_B, PI_I_B, PI_IMIN_B, PI_IMAX_B);
+
+void setup()
+{
   // Inicjalizacja pinów dla sterowania silnikami
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -77,7 +94,7 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
-  
+
   // Inicjalizacja pinów enkoderów
   pinMode(ENCODER_PIN1, INPUT_PULLUP);
   pinMode(ENCODER_PIN2, INPUT_PULLUP);
@@ -93,11 +110,22 @@ void setup() {
   // Inicjalizacja komunikacji szeregowej
   Serial.begin(9600);
   Serial.setTimeout(1000000000);
+
+  // Ustawienie kierunku obrotów silników
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
-#define TARGET_PULSES 50
+#define TARGET_PULSES 20
 
-void loop() {
+long lastTimePrinted = 0;
+int prev_pwmA=0;
+int prev_pwmB=0;
+
+void loop()
+{
   // Aktualny czas
   unsigned long now = millis();
   // Obliczenie czasu od ostatniego odczytu
@@ -105,54 +133,61 @@ void loop() {
   lastTime = now;
 
   // Obliczenie błędu regulacji dla obu enkoderów
-  uint16_t error1 = TARGET_PULSES - currentPosition1;
-  uint16_t error2 = TARGET_PULSES - currentPosition2;
+  int16_t error1 = TARGET_PULSES - currentPosition1;
+  int16_t error2 = TARGET_PULSES - currentPosition2;
 
+  int pwmA = piA.update(error1, dt);
+  int pwmB = piB.update(error2, dt);
   // Ustawienie ograniczeń dla sygnału sterującego PWM
-  // pwm1 = constrain(pwm1, 100, 255); // Ograniczenie PWM do mniejszych wartości
-  // pwm2 = constrain(pwm2, 100, 255); // Ograniczenie PWM do mniejszych wartości
+  // pwmA = constrain(pwmA, 180, 255); // Ograniczenie PWM do mniejszych wartości
+  // pwmB = constrain(pwmB, 190, 255); // Ograniczenie PWM do mniejszych wartości
 
-  // String pwmCmd = Serial.readStringUntil('\r');
-  // int pwm=pwmCmd.toInt();
-  // analogWrite(ENA, pwm);
-  // analogWrite(ENB, pwm+10);
-  // Serial.println(pwm);
+  if (pwmA < 0) dirA=-1; else dirA=1;
+  if (pwmB < 0) dirB=-1; else dirB=1;
+
+  pwmA += 180/(1.34) * dirA;
+  pwmB += 190/(1.34) * dirB;
+
+  pwmA = 0.5*prev_pwmA + 0.5*pwmA;
+  pwmB = 0.5*prev_pwmB + 0.5*pwmB;
+
+  prev_pwmA = pwmA;
+  prev_pwmB = pwmB;
+
+  pwmA = constrain(pwmA, -255, 255);
+  pwmB = constrain(pwmB, -255, 255);
+
+  digitalWrite(IN1, pwmA<0);
+  digitalWrite(IN2, pwmA>0);
+  digitalWrite(IN3, pwmB<0);
+  digitalWrite(IN4, pwmB>0);
 
   // Ustawienie wypełnienia sygnału PWM na podstawie wyniku regulatora PID
-  // analogWrite(ENA, abs(pwm1));
-  // analogWrite(ENB, abs(pwm2));
-
-  // Ustawienie kierunku obrotów silników
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-
+  analogWrite(ENA, abs(pwmA));
+  analogWrite(ENB, abs(pwmB));
 
   // Wyświetlenie obecnej pozycji i błędu
-  // Serial.print("Imp1: ");
-  // Serial.print(newPosition1);
-  // Serial.print("\t");
-  // Serial.print("Imp2: ");
-  // Serial.println(newPosition2);
+  if (millis() - lastTimePrinted > 500)
+  {
+    Serial.print("Imp1: ");
+    Serial.print(currentPosition1);
+    Serial.print("\t");
+    Serial.print("Imp2: ");
+    Serial.print(currentPosition2);
+    Serial.print("\t\t");
+    Serial.print("errA: ");
+    Serial.print(error1);
+    Serial.print("\t");
+    Serial.print("pwmA: ");
+    Serial.print(pwmA);
+    Serial.print("\t");
+    Serial.print("errB: ");
+    Serial.print(error2);
+    Serial.print("\t");
+    Serial.print("pwmB: ");
+    Serial.println(pwmB);
+    lastTimePrinted = millis();
+  }
 
-// Sprawdzenie, czy cel został osiągnięty
-// if (newPosition1 * MM_PER_PULSE >= targetDistance && newPosition2 * MM_PER_PULSE >= targetDistance) {
-//     analogWrite(ENA, 0);
-//     analogWrite(ENB, 0);
-//     digitalWrite(IN1, LOW);
-//     digitalWrite(IN2, LOW);
-//     digitalWrite(IN3, LOW);
-//     digitalWrite(IN4, LOW);
-//     Serial.println("Target distance reached!");
-//     while (true) {
-//       Serial.print("Imp1: ");
-//       Serial.print(newPosition1);
-//       Serial.print("\t");
-//       Serial.print("Imp2: ");
-//       Serial.println(newPosition2);
-//     } // Zatrzymanie pętli głównej
-// }
-
-  // delay(100); // Delikatne opóźnienie dla stabilności odczytu
+  delay(10); // Delikatne opóźnienie dla stabilności odczytu
 }
